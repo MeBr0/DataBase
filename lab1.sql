27114ed9044465cbc8b54d4ed59fa1de3ede31a0
mebr0 <aza17e@gmail.com>
2018-09-14 14:08:39 +0600

    first lab

---
+++ lab1.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE lab1;
+
+CREATE TABLE users(
+  id SERIAL,
+  firstname VARCHAR(50),
+  lastname VARCHAR(50)
+);
+
+DROP TABLE users;
+
+-- SELECT * FROM users;
+
+ALTER TABLE users ADD COLUMN isadmin INT;
+
+ALTER TABLE users ALTER COLUMN isadmin DROP DEFAULT;
+
+ALTER TABLE users ALTER isadmin TYPE bool USING isadmin::boolean;
+
+ALTER TABLE users ALTER COLUMN isadmin SET DEFAULT FALSE;
+
+ALTER TABLE users ADD CONSTRAINT qwe PRIMARY KEY(id);
+
+CREATE TABLE tasks(
+  id SERIAL,
+  name VARCHAR(50),
+  user_id INT
+);
+
+DROP TABLE tasks;
+
+DROP DATABASE lab1;
